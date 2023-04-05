39365df0c941d12ae5c88a7d1636fe948c1ffc21
uniqcle <uniqcle@yandex.ru>
2023-04-05 13:36:18 +0700

    toggle mobile nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const navBtn = document.querySelector('.mobile-nav__button');
 const mobileNav = document.querySelector('.mobile-nav');
 
-navBtn.addEventListener('click', function () {
+
+navBtn.addEventListener('click', function (e) {
+    e.stopPropagation();
     mobileNav.classList.toggle('mobile-nav--active')
     navBtn.classList.toggle('mobile-nav__button-close');
+})
+
+
+window.addEventListener('click', function () {
+
+    if (mobileNav.classList.contains('mobile-nav--active')) {
+
+        mobileNav.classList.toggle('mobile-nav--active')
+        navBtn.classList.toggle('mobile-nav__button-close');
+
+    }
+})
+
+mobileNav.addEventListener('click', function (e) {
+    e.stopPropagation();
+
 })
